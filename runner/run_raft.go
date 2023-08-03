a11f86b0b0c44b55d2324faaa4627e3191e8f2a4
JohanLi2333 <k20020910@kcl.ac.uk>
2023-08-03 20:15:14 +0100

    progress

--- runner/run_raft.go
+++ runner/run_raft.go
@@ -13,8 +13,8 @@ func main() {
 	clients := []*client.Client{}
 	cl1 := client.MakeClient("192.168.0.7")
 	clients = append(clients, cl1)
-	// cl2 := client.MakeClient("192.168.0.109")
-	// clients = append(clients, cl2)
+	cl2 := client.MakeClient("192.168.0.109")
+	clients = append(clients, cl2)
 	// applyCh := make(chan raft.ApplyMsg)
 	persister := raft.MakePersister()
 	me := 0
