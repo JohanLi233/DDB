68a49ba11a4b4f51e5e52c358baecadcbb1e97e2
JohanLi2333 <k20020910@kcl.ac.uk>
2023-08-03 20:14:54 +0100

    progress

--- runner/run_raft.go
+++ runner/run_raft.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"6.5840/client"
+	"6.5840/kvraft"
 	"6.5840/raft"
 )
 
@@ -12,13 +13,13 @@ func main() {
 	clients := []*client.Client{}
 	cl1 := client.MakeClient("192.168.0.7")
 	clients = append(clients, cl1)
-	cl2 := client.MakeClient("192.168.0.109")
-	clients = append(clients, cl2)
-	applyCh := make(chan raft.ApplyMsg)
+	// cl2 := client.MakeClient("192.168.0.109")
+	// clients = append(clients, cl2)
+	// applyCh := make(chan raft.ApplyMsg)
 	persister := raft.MakePersister()
 	me := 0
 	time.Sleep(1 * time.Second)
-	rf := raft.Make(clients, me, persister, applyCh)
-	fmt.Println(rf)
+	kvrf := kvraft.StartKVServer(clients, me, persister, -1)
+	fmt.Println(kvrf)
 	time.Sleep(100 * time.Second)
 }
