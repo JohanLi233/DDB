f2d346077989e20e40599a403fce54e6e5c5270d
JohanLi2333 <k20020910@kcl.ac.uk>
2023-08-04 15:58:01 +0100

    progress

--- main/run_server.go
+++ main/run_server.go
@@ -17,7 +17,7 @@ func main() {
 	clients = append(clients, cl2)
 	persister := raft.MakePersister()
 	me := 0
-	kv := kvraft.StartKVServer(clients, me, persister, 100)
+	kv := kvraft.StartKVServer(clients, me, persister, -1)
 	fmt.Println("ok")
 	for !kv.Killed() {
 		time.Sleep(100 * time.Minute)
